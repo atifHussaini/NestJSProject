74403f306f1942dacee6280ce2ebc6d58e47ae0c
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-11-16 14:49:23 -0600

    property, region and app module worked on

--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PropertyController } from './property.controller';
 import { PropertyService } from './property.service';
+import { Property } from './property.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Property])],
   controllers: [PropertyController],
   providers: [PropertyService],
 })
