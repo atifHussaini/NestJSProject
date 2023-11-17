6b949cb8ef8caa89a29b80e46b9c564f54cc766d
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-11-16 19:20:41 -0600

    lead module

--- src/lead/lead.module.ts
+++ src/lead/lead.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LeadController } from './lead.controller';
 import { LeadService } from './lead.service';
+import { Lead } from './lead.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Lead])],
   controllers: [LeadController],
-  providers: [LeadService]
+  providers: [LeadService],
 })
 export class LeadModule {}
